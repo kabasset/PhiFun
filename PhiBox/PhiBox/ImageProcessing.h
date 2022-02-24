7df755c004842ac1d372aa5a3ff83ef2e91eb66c
Antoine Basset <antoine.basset@cnes.fr>
2022-02-24 11:31:04 +0100

    Enable 2D interpolation for nD rasters

--- PhiBox/PhiBox/ImageProcessing.h
+++ PhiBox/PhiBox/ImageProcessing.h
@@ -10,22 +10,27 @@
 namespace Phi {
 namespace Image2D {
 
-template <typename U, typename TRaster>
-U bilinear(const TRaster& raster, double x, double y) {
+template <typename T>
+T clamp(T in, T min, T max) {
+  return std::max(min, std::min(in, max));
+}
+
+template <typename U, typename TRaster, typename... TLongs>
+U bilinear(const TRaster& raster, double x, double y, TLongs... coords) {
   using T = typename TRaster::Value;
-  const long left = x < 0 ? 0 : x;
-  const long bottom = y < 0 ? 0 : y;
-  const T* bl = &raster[{left, bottom}];
+  const auto width = raster.shape()[0];
+  const auto height = raster.shape()[1];
+  const auto left = clamp<long>(x, 0, width - 1);
+  const auto bottom = clamp<long>(y, 0, height - 1);
+  const T* bl = &raster[{left, bottom, coords...}];
   if (x == left && y == bottom) { // No interpolation
     return *bl;
   }
   const double dx = x - left;
   const double dy = y - bottom;
-  const long width = raster.template length<0>();
-  const long height = raster.template length<1>();
-  const T* br = x >= width ? bl : bl + 1;
-  const T* tl = y >= height ? bl : bl + width;
-  const T* tr = x >= width ? tl : tl + 1;
+  const T* br = x >= width - 1 ? bl : bl + 1;
+  const T* tl = y >= height - 1 ? bl : bl + width;
+  const T* tr = x >= width - 1 ? tl : tl + 1;
   return *bl + (*br - *bl + (*bl + *tr - *br - *tl) * dy) * dx + (*tl - *bl) * dy;
 }
 
